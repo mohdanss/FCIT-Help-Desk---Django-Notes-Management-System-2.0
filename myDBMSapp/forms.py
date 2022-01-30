c3f2f641c4bef7824ff894f36b2da81615ee75b3
muhammad1244 <anstig3r@gmail.com>
2022-01-30 17:36:27 +0500

    Uploaded the updated project

---
+++ myDBMSapp/forms.py
@@ -0,0 +1,13 @@
+
+from django import forms 
+from .models import *
+
+class profilePic(forms.ModelForm): 
+	class Meta: 
+		model = Register 
+		fields = ['profile_pic', ] 
+
+class contributionFile(forms.ModelForm): 
+	class Meta: 
+		model = Contributions 
+		fields = ['contribution_file', ] 
